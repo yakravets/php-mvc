d649b7352f68876921a322112606f66a5c9062ef
Yaroslav <yakravets@ktc.rovno.ua>
2020-01-30 11:33:37 +0200

    configuration ignoring temp files and folders

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 require_once 'app/lib/autoload.php';
-require_once 'config.php';
+if (file_exists('config.php')) {
+    require_once 'config.php';
+}
+else {
+    echo 'Rename file \"config-simple.php\" to \"config.php\" and fill settings\n';
+    exit();
+}
+
 
 use app\core\Router;
 
